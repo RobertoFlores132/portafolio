430ea711a35dac3bfc6570631aae9409135126f9
RobertoFlores132 <crystalgato@yahoo.com>
2023-04-17 18:49:10 -0600

    Title and icon added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Aboutme from './components/Aboutme';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { Helmet } from 'react-helmet';
 
 function App() {
+
   return (
       <div className='App'>
+         <Helmet>
+                <meta charSet="utf-8" />
+                <title>Roberto's Portfolio</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+                <meta name="description" content="Work Portfolio" />
+            </Helmet>
         <div id='Home'>
           <Home/>
         </div>
