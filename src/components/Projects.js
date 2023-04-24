77b701097bd4b3142d4cd1241452268c4f0647e6
RobertoFlores132 <crystalgato@yahoo.com>
2023-04-24 13:29:48 -0600

    Fixed some issues

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,7 +27,10 @@ const Projects = () => {
             </div>
 
             <div className='descriptioncontainer2'>
-           
+            <div className='proyect-button2'>
+            <a className='button1' href='https://wonderful-haibt-5416e9.netlify.app/' target="_blank" rel="noopener noreferrer" >Visitar Sitio</a>
+            <a className='button1' href='https://github.com/RobertoFlores132/WeatherApp' target="_blank" rel="noopener noreferrer" >Ver repositorio</a>
+            </div>
             <div className='proyect-description2'>
                 <h3>Weather App</h3>
                 <p>Página que muestra el clima en grados Fahrenheit y Celsius, también detecta la ubicación
@@ -35,13 +38,9 @@ const Projects = () => {
             </div>
             <div className='proyect-image2'>
                 <img className='Proyect2' src={Project2} alt="Weather App" />
-            </div> 
-            <div className='proyect-button2'>
-            <a className='button1' href='https://wonderful-haibt-5416e9.netlify.app/' target="_blank" rel="noopener noreferrer" >Visitar Sitio</a>
-            <a className='button1' href='https://github.com/RobertoFlores132/WeatherApp' target="_blank" rel="noopener noreferrer" >Ver repositorio</a>
             </div>
+            
             </div>
-
             <div className='descriptioncontainer'>
             <div className='proyect-image1'>
                 <img className='Proyect1' src={Project3} alt="Gym app" />
