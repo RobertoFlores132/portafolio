370c5f509177e600d2b7675e8b8414efa95c6f4b
RobertoFlores132 <crystalgato@yahoo.com>
2023-04-24 13:20:52 -0600

    Fixed project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,23 +27,24 @@ const Projects = () => {
             </div>
 
             <div className='descriptioncontainer2'>
-            <div className='proyect-button2'>
-            <a className='button1' href='https://wonderful-haibt-5416e9.netlify.app/' target="_blank" rel="noopener noreferrer" >Visitar Sitio</a>
-            <a className='button1' href='https://github.com/RobertoFlores132/WeatherApp' target="_blank" rel="noopener noreferrer" >Ver repositorio</a>
-            </div>
+           
             <div className='proyect-description2'>
                 <h3>Weather App</h3>
                 <p>Página que muestra el clima en grados Fahrenheit y Celsius, también detecta la ubicación
                 </p>
             </div>
             <div className='proyect-image2'>
-                <img className='Proyect2' src={Project2} alt="Rick And Morty API" />
+                <img className='Proyect2' src={Project2} alt="Weather App" />
+            </div> 
+            <div className='proyect-button2'>
+            <a className='button1' href='https://wonderful-haibt-5416e9.netlify.app/' target="_blank" rel="noopener noreferrer" >Visitar Sitio</a>
+            <a className='button1' href='https://github.com/RobertoFlores132/WeatherApp' target="_blank" rel="noopener noreferrer" >Ver repositorio</a>
             </div>
             </div>
 
             <div className='descriptioncontainer'>
             <div className='proyect-image1'>
-                <img className='Proyect1' src={Project3} alt="Rick And Morty API" />
+                <img className='Proyect1' src={Project3} alt="Gym app" />
             </div>
             <div className='proyect-description'>
                 <h3>Gym Page</h3>
@@ -52,8 +53,8 @@ const Projects = () => {
                 </p>
             </div>
             <div className='proyect-button'>
-            <a className='button1' href='https://fervent-blackwell-cc377f.netlify.app/' target="_blank" rel="noopener noreferrer" >Visitar Sitio</a>
-            <a className='button1' href='https://github.com/RobertoFlores132/RickAndMortyAPI' target="_blank" rel="noopener noreferrer" >Ver repositorio</a>
+            <a className='button1' href='https://amazing-sprite-fa04df.netlify.app' target="_blank" rel="noopener noreferrer" >Visitar Sitio</a>
+            <a className='button1' href='https://github.com/RobertoFlores132/gymapp' target="_blank" rel="noopener noreferrer" >Ver repositorio</a>
             </div>
             </div>
 
